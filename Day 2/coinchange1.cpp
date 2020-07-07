41cecc21db747dec4fc12b7a7e6223c8dd6f863e
pranavnkps <pranavnkps.181co239@nitk.edu.in>
2020-07-07 22:10:06 +0400

    Coin Change I

--- Day 2/coinchange1.cpp
+++ Day 2/coinchange1.cpp
@@ -1,3 +1,4 @@
+//SOLUTION 1
 class Solution {
 public:
     int coinChange(vector<int>& coins, int amount) {
@@ -10,7 +11,7 @@ public:
         for(int i = 1;i<=amount;++i){
             min = INT_MAX;
                 for(int x:coins){
-                    if((i-x) < 0 || dp[i-x] == -1){
+                    if((i-x) < 0 || dp[i-x] == -1){ //if you cannot form change for dp[i-x], then not possible for dp[i] also
                         dp[i] = -1;
                         continue;
                     }
@@ -23,6 +24,33 @@ public:
                     dp[i] = min;
 
             }
+        return dp[amount];
+    }
+};
+
+//SOLUTION 2 - MORE UNDERSTANDABLE
+class Solution {
+public:
+    int coinChange(vector<int>& coins, int amount) {
+        if(amount == 0)
+            return 0;
+
+        int min;
+        vector<int> dp(amount+1, amount+1);
+        dp[0] = 0;
+        for(int i = 1;i<=amount;++i){
+
+                for(int x:coins){
+
+                    if(i>=x){
+                    if((dp[i-x]+1) < dp[i])
+                        dp[i] = dp[i-x]+1;
+                    }
+                }
+
+            }
+        if(dp[amount]>amount)
+            return -1;
         return dp[amount];
     }
 };
