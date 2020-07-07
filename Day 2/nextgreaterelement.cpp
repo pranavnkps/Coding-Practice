ed59fcfc3eaf836c7dd3047900378cca4931bff1
pranavnkps <pranavnkps.181co239@nitk.edu.in>
2020-07-07 17:37:23 +0400

    Solution using Stack Added

--- Day 2/nextgreaterelement.cpp
+++ Day 2/nextgreaterelement.cpp
@@ -19,3 +19,41 @@ public:
         return res;
     }
 };
+
+//SOLUTION 2 - USING STACK
+class Solution {
+public:
+    vector<int> nextGreaterElements(vector<int>& nums) {
+        vector<int> res(nums.size());
+        stack<int> s;
+        for(int i = nums.size()-1; i>=0; --i){
+            while(!s.empty() && nums[s.top()] <= nums[i]){
+                //res[s.top()] = nums[i];
+                s.pop();   
+            }
+            if(s.empty()){
+                res[i] = -1;
+                s.push(i);
+            }
+            else{
+                res[i] = nums[s.top()];
+                s.push(i);
+            }
+        }
+        for(int i = nums.size()-1; i>=0; --i){
+            while(!s.empty() && nums[s.top()] <= nums[i]){
+               // res[s.top()] = nums[i];
+                s.pop();   
+            }
+            if(s.empty()){
+                res[i] = -1;
+                s.push(i);
+            }
+            else{
+                res[i] = nums[s.top()];
+                s.push(i);
+            }
+        }
+        return res;
+    }
+};
